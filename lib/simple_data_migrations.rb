57aa5c68810d8d3ef36e7a6ccb096cd3504b32d6
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2022-01-06 09:55:09 +0100

    Gem init

---
+++ lib/simple_data_migrations.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "simple_data_migrations/version"
+
+module SimpleDataMigrations
+  class Error < StandardError; end
+  # Your code goes here...
+end
